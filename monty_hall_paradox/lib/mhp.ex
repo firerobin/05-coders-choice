9bf7f6fc17d53a7877c10b69f0bd119f898ac8f3
firerobin <30886619+firerobin@users.noreply.github.com>
2017-11-26 01:53:56 -0600

    feature-1

--- monty_hall_paradox/lib/mhp.ex
+++ monty_hall_paradox/lib/mhp.ex
@@ -1,12 +1,10 @@
 defmodule Mhp do
 
-  alias Mhp.Supervisor
-  alias Mhp.Server
-  
-  defdelegate new_game(),                     to: Supervisor
-  defdelegate sim_game(),                     to: Supervisor
-  defdelegate select_door(game, number),      to: Api
-  defdelegate stay_or_switch?(game, choice),  to: Api
-  defdelegate tally(game),                    to: Api
+  alias Mhp.Api
+
+  defdelegate new_game(),               to: Api
+  defdelegate select_door(number),      to: Api
+  defdelegate stay_or_switch?(choice),  to: Api
+  defdelegate tally(),                  to: Api
 
 end
